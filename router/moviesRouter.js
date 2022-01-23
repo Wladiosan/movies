c9a45584bdd795e141bb5f0e2c042e3372406510
Wladiosan <w.ohrimenko@gmail.com>
2022-01-23 16:39:39 +0200

    fixed error

--- router/moviesRouter.js
+++ router/moviesRouter.js
@@ -6,7 +6,7 @@ const router = new Router()
 
 router.post('/', MoviesController.create)
 router.delete('/:id', MoviesController.delete)
-router.path('/:id', MoviesController.update)
+router.patch('/:id', MoviesController.update)
 router.get('/:id', MoviesController.show)
 router.get('/', MoviesController.list)
 router.post('/import', MoviesController.import)
